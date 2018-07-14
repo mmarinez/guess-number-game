a249a932221366ddb8e00ce648489c9bc1f37808
Manuel Marinez <marinezdev@gmail.com>
2018-07-14 14:59:28 -0400

    Add player input

--- game_main.py
+++ game_main.py
@@ -1,3 +1,11 @@
+import random
+
 print("-------------------")
 print("  Guess Game Start")
 print("-------------------")
+
+number_to_guess = random.randint(0,10)
+
+player_number = input("Guess the number between 0 to 10\n")
+
+player = int(player_number)
